5356885162373da5ec1bb59e1521109b7346284b
KorNito <kornelijuspacevicius@gmail.com>
2021-07-04 12:09:35 +0300

    localstorage

--- app.js
+++ app.js
@@ -10,24 +10,7 @@ class Pizza {
 
 class UI {
   static displayPizzas() {
-    const storedPizzas = [
-      {
-        name: "Pizza1",
-        price: 1.0,
-        heat: 2,
-        toppings: ["tomato, cheese"],
-        photo: "",
-      },
-      {
-        name: "Pizza2",
-        price: 2.0,
-        heat: 3,
-        toppings: ["cheese, salami"],
-        photo: "",
-      },
-    ];
-
-    const pizzas = storedPizzas;
+    const pizzas = Storage.getPizzas();
 
     pizzas.forEach((pizza) => UI.addPizza(pizza));
   }
@@ -42,7 +25,7 @@ class UI {
     pizzaLi.classList.add("pizza-item");
 
     pizzaLi.innerHTML = `
-        <p>${pizza.name}<p/>
+        <p class="pizza-name">${pizza.name}<p/>
         <p>${pizza.price}<p/>
         <p>${pizza.heat}<p/>
         <p>${pizza.toppings}<p/>
@@ -77,6 +60,39 @@ class UI {
   };
 }
 
+class Storage {
+  static getPizzas() {
+    let pizzas;
+    if (localStorage.getItem("pizzas") === null) {
+      pizzas = [];
+    } else {
+      pizzas = JSON.parse(localStorage.getItem("pizzas"));
+    }
+
+    return pizzas;
+  }
+
+  static addPizza(pizza) {
+    const pizzas = Storage.getPizzas();
+
+    pizzas.push(pizza);
+
+    localStorage.setItem("pizzas", JSON.stringify(pizzas));
+  }
+
+  static removePizza(name) {
+    const pizzas = Storage.getPizzas();
+
+    pizzas.forEach((pizza, index) => {
+      if (pizza.name === name) {
+        pizzas.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem("pizzas", JSON.stringify(pizzas));
+  }
+}
+
 document.addEventListener("DOMContentLoaded", UI.displayPizzas);
 
 document.querySelector(".form").addEventListener("submit", (event) => {
@@ -99,10 +115,16 @@ document.querySelector(".form").addEventListener("submit", (event) => {
 
     UI.addPizza(pizza);
 
+    Storage.addPizza(pizza);
+
     UI.clearFormInputs();
   }
 });
 
 document.querySelector(".pizza-list").addEventListener("click", (event) => {
   UI.removePizza(event.target);
+
+  Storage.removePizza(
+    event.target.previousElementSibling.children[0].textContent
+  );
 });
